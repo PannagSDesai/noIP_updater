e0ee608188eea3f0d12276f6640a6db5fd7e0f95
Pannag S <34961641+PannagSDesai@users.noreply.github.com>
2022-02-16 18:55:36 +0530

    Update noIP_updater.py

--- noIP_updater.py
+++ noIP_updater.py
@@ -9,16 +9,17 @@ import logging
 from requests import get
 import time
 last_ip = ""
+update_frequency = 60 #check and update frequency in Seconds. By Default every 60 seconds
 
-#No IP credentials
+#No-IP credentials and Hostname associated with the No-IP account
 username = ""
 password = ""
-
+hostname = ""  
 while (1):
     ip = get('https://api.ipify.org').text
     print(f'My public IP address is: {ip}')
     if last_ip != ip:
-        resp = get(f'http://{username}:{password}@dynupdate.no-ip.com/nic/update?hostname=halbch-auto.ddns.net&myip={ip}').text
+        resp = get(f'http://{username}:{password}@dynupdate.no-ip.com/nic/update?hostname={hostname}&myip={ip}').text
         last_ip = ip
         if  "nochg" not in resp or "good" not in resp:
             fh = logging.FileHandler('noIP_errors.log')
